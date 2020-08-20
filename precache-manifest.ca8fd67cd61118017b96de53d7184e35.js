a5c27a0c57ec8aeccbdb758c99deb0e5df008e55
Lucas Zago <lcardosozago@gmail.com>
2020-08-20 17:02:09 -0400

    Updates

---
+++ precache-manifest.ca8fd67cd61118017b96de53d7184e35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772fa7703799c6f08c3b938f97cb23ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d248c1584305570bf51",
+    "url": "/static/css/main.147da490.chunk.css"
+  },
+  {
+    "revision": "dcb04fa7695f1d8e2079",
+    "url": "/static/js/2.e8da8071.chunk.js"
+  },
+  {
+    "revision": "be791ed72a93ab8353716f0f3e3255ab",
+    "url": "/static/js/2.e8da8071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d248c1584305570bf51",
+    "url": "/static/js/main.31c5d155.chunk.js"
+  },
+  {
+    "revision": "d5965f01dc833bb5d871",
+    "url": "/static/js/runtime-main.5b5f0df6.js"
+  },
+  {
+    "revision": "43908fabbc4ba7df40df0881bb5cd3d8",
+    "url": "/static/media/microdados.43908fab.csv"
+  },
+  {
+    "revision": "cfea6544b53aceb186688c5d59b51a86",
+    "url": "/static/media/ms_map.cfea6544.svg"
+  }
+]);
