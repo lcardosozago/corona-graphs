65ad4b2916c1d0005c74040c39625b50f48e4345
Lucas Zago <lcardosozago@gmail.com>
2020-12-19 10:47:30 -0400

    Updates

---
+++ precache-manifest.e070cbcb761c556402c8fe0d488a3ee1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35e93fde32714b87a1821301d445807",
+    "url": "/corona-graphs/index.html"
+  },
+  {
+    "revision": "a2d5ddc77b182da9686e",
+    "url": "/corona-graphs/static/css/main.39bf29ed.chunk.css"
+  },
+  {
+    "revision": "d02f7a3890f939da2c6f",
+    "url": "/corona-graphs/static/js/2.ec074edb.chunk.js"
+  },
+  {
+    "revision": "e684bc17ac1b4e0de3548bd49f69784a",
+    "url": "/corona-graphs/static/js/2.ec074edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d5ddc77b182da9686e",
+    "url": "/corona-graphs/static/js/main.5b22a0f5.chunk.js"
+  },
+  {
+    "revision": "de5f24bd765f9c1ddd6c",
+    "url": "/corona-graphs/static/js/runtime-main.8a64ffa9.js"
+  },
+  {
+    "revision": "55d7a4e8ff662cb8bee49a202285d25d",
+    "url": "/corona-graphs/static/media/microdados.55d7a4e8.csv"
+  },
+  {
+    "revision": "882fbecc2876507b2e721cf050786821",
+    "url": "/corona-graphs/static/media/ms_map.882fbecc.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-graphs/precache-manifest.e56fbe3e8c6d8e947ebb6df7a681d933.js"
+  "/corona-graphs/precache-manifest.e070cbcb761c556402c8fe0d488a3ee1.js"
 );
 
 self.addEventListener('message', (event) => {
