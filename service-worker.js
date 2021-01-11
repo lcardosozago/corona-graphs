e7c293d6b381836dfe7fe2182dd5ebb3ea4a1e12
Lucas Zago <lcardosozago@gmail.com>
2021-01-11 12:47:40 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-graphs/precache-manifest.a88e8d962c9de4027e630874d77bee5a.js"
+  "/corona-graphs/precache-manifest.db02cb22c494e44117d074030eaa89f6.js"
 );
 
 self.addEventListener('message', (event) => {
